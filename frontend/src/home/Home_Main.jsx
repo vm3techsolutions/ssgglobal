d9b7354e1041fb274fa0157b51d3486d8ea6466f
poojaraut2618 <poojaraut2618@gmail.com>
2025-03-07 09:51:23 +0530

    change form path in home file

--- frontend/src/home/Home_Main.jsx
+++ frontend/src/home/Home_Main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import Banner from './Banner';
 import About_Us from './About_Us';
 import Puja from './Puja';
 // import Testimonials from './Testimonials';
 import './Home_Main.css';
-import Form from '../Contact/Form';
+import Form from '../contact/Form';
 // import Img4 from './Img4';
 import HomeGetTouch from './HomeGetTouch';
 import BlogSlider from './BlogSlider';
