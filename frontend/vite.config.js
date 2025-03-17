f55dbda5a54598791bfbba01fe562bb9374b9d5b
poojaraut2618 <poojaraut2618@gmail.com>
2025-03-17 11:43:22 +0530

    add allowed host in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // Ensure Tailwind is NOT being imported here (it's handled in postcss)
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
+  preview: {
+    allowedHosts: ["saishaktisant.org"],
+  },
 });
